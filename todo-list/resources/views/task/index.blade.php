62ae5538b9379cb50a42627ab3df02d5f1057baf
pablojwittich <pablojwittich1998@gmail.com>
2024-10-01 18:26:28 -0300

    add form

--- todo-list/resources/views/task/index.blade.php
+++ todo-list/resources/views/task/index.blade.php
@@ -1,1 +1,7 @@
 @include('app')
+<body>
+    <form action="{{'/'}}" method="post">
+        <input type="text" name="task" id="task">
+        <input type="submit" value="Agregar tarea">
+    </form>
+</body>
