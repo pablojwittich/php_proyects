953bee54ffc5f149c2496f83222d40cb2302d2f2
pablojwittich <pablojwittich1998@gmail.com>
2024-10-04 09:03:47 -0300

    views

--- page_ato/views/app.php
+++ page_ato/views/app.php
@@ -9,15 +9,38 @@
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sq
     </head>
     <body>
-        <header>
-            <nav>
-                <ul>
-                    <li><a href="index.php"><h1>A tus ordenes</h1></a></li>
-                    <li><a href="quienes.php">Quienes somos</a></li>
-                    <li><a href="vision.php">Visión</a></li>
-                    <li><a href="mision.php">Mision</a></li>
-                </ul>
-            </nav>
+        <header style="color: black;">
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+          <div class="container-fluid">
+            <a class="navbar-brand" href="index.php">A tus ordenes</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+              <ul class="navbar-nav">
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="quienes.php">Quienes somos</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="vision.php">Vision</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="mision.php">Mision</a>
+                </li>
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Dropdown link
+                  </a>
+                  <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">Action</a></li>
+                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
         </header>
     </body>
 </html>
