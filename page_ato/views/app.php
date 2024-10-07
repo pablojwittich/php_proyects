fb193e0a11c55178e5257cfe865b834abb869103
pablojwittich <pablojwittich1998@gmail.com>
2024-10-07 18:59:53 -0300

    new page the atusordenes.com.ar with php and mvc != wordpress

--- page_ato/views/app.php
+++ page_ato/views/app.php
@@ -5,45 +5,43 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>A tus ordenes</title>
+        <link rel="shortcut icon" src="../views/resources/LocoComunidadApostolica-1.png">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sq
     </head>
-    <body style="width:85%; display:flex; justify-content: center">
+    <body>
+        <section style="display: block; justify-content: center; align-items: center; width: 90%">
         <header>
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
-          <div class="container-fluid">
+          <div class="container-fluid"
             <a href="index.php">
-                <img id="logo" src="resources/LocoComunidadApostolica-1.png" style="width: 3em; heith:3em; border-radius:50px; margin:0.5em 0.5em 0.5em 0.5em">
+                <img src="resources/LocoComunidadApostolica-1.png" style="width: 3em; heith:3em; border-radius:50px; margin:0.5em 0.5em 0.5em 0.5em">
             </a>
-            <a class="navbar-brand" href="index.php" style="color:black; ">A tus ordenes</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavDropdown">
-              <ul class="navbar-nav" style="margin-left:35em;">
-                <li class="nav-item">
+            <a  href="index.php" style="color:black;">A tus ordenes</a>
+              <ul class="navbar-nav" style="margin-left:48em;">
+                <li>
                   <a class="nav-link active" aria-current="page" href="quienes.php" style="color:black">Quienes somos</a>
                 </li>
-                <li class="nav-item">
+                <li>
                   <a class="nav-link" href="vision.php" style="color:black">Vision</a>
                 </li>
-                <li class="nav-item">
+                <li>
                   <a class="nav-link" href="mision.php" style="color:black" >Mision</a>
                 </li>
-                <li class="nav-item dropdown">
-                  <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" style="color:black">
-                    Dropdown link
-                  </a>
-                  <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="#">Action</a></li>
-                    <li><a class="dropdown-item" href="#">Another action</a></li>
-                    <li><a class="dropdown-item" href="#">Something else here</a></li>
-                  </ul>
+                <li>
+                    <a class="nav-link"  href="inicio_sesion.php" style="color:black; adding: 10px 20px; background-color: #00aae4; color: black; border-radius: 5px; text-decoration: none;" >Iniciar Sesion</a>
                 </li>
+                </ul>
               </ul>
             </div>
           </div>
         </nav>
         </header>
+        </section>
+        <footer style="vertical-align: bottom; text-align:center ;position: fixed; left: 0; bottom: 0; width: 100%;">
+          <hr style="width:90% ;">
+          <p>&copy; Copyright 2024 Indelt. Todos los derechos reservados.</p>
+          <p>Pagina creada por <a href="https://github.com/pablojwittich" target="_blank">Pablo J. Wittich</a></p>
+        </footer>
     </body>
 </html>
--- page_ato/views/index.php
+++ page_ato/views/index.php
@@ -1,3 +1,24 @@
 <?php
-include('app.php');
-?>
+include "app.php"; ?>
+
+<section>
+    <article>
+    <h1>Comunidad Apostolica San Isidoro de Sevilla</h2>
+    <p>
+        <cite>"Debes compartir lo que sabes y además lo que aprendes y así la sabiduría podrá alcanzarte"</cite>
+    </p>
+    </article>
+    <picture>
+        <img src="/resources/Isidor_von_Sevilla-edited-3.jpeg" alt="San Isidoro de Sevilla">
+    </picture>
+</section>
+<script>
+    <style>
+        picture {
+            width: 50%;
+        }
+        article {
+            width: 50%;
+        }
+    </style>
+</script>
