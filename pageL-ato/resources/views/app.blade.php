83a780302b56f56b5fa28104fb47b992d92dfa66
pablojwittich <pablojwittich1998@gmail.com>
2024-10-09 22:23:42 -0300

    copy project page_ato in Laravel

---
+++ pageL-ato/resources/views/app.blade.php
@@ -0,0 +1,57 @@
+<!Doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>A tus ordenes!</title>
+    <style>
+        body {
+            width: 90%;
+            background-color: #f2f2f2;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+        nav{
+            background-color: #1f5cc5;
+            margin-top: 1em;
+            border-radius: 40px;
+            padding-top: 1em;
+            pading-bottom: 1em;
+            display: flex;
+            align-items: center;
+        }
+        a{
+            padding-right: 1em;
+            padding-left: 1em;
+            padding-bottom: 1em;
+            color: white;
+            line-height: 1.5em;
+            vertical-align: center;
+        }
+        .logo{
+            border-radius: 50%;
+            width: 2em;
+            height: 2em;
+
+        }
+    </style>
+</head>
+<body>
+    <section>
+        <header>
+            <nav>
+                <a><img class="logo" src="//home/p4bl0/Documentos/php_proyects/pageL-ato/resources/img/logo_ca.png" alt="logo_CA"/></a>
+                <a  href="#">A tus ordenes!</a>
+                <a href="#">Home</a>
+                <a href="#">Features</a>
+                <a href="#">Pricing</a>
+                <a href="" >Disabled</a>
+            </nav>
+        </header>
+    </section>
+</body>
+</html>
