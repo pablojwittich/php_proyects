caaa912dccf5fb75d78a5829a36e280861c1f9ef
pablojwittich <pablojwittich1998@gmail.com>
2024-10-16 18:13:55 -0300

    login page ato

---
+++ page-ato/controllers/controller-sign-in.php
@@ -0,0 +1,5 @@
+<?php
+include("../models/connection.php");
+
+
+?>
---
+++ page-ato/models/connection.php
@@ -0,0 +1,10 @@
+<?php
+$servername = "127.0.0.1";
+$username = "root";
+$password = "123";
+$database = "ato_db";
+
+$conn = new mysqli($servername, $username, $password, $database);
+$conn->set_charset("utf8");
+
+?>
