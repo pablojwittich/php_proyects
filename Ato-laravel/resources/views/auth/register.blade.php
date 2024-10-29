e0df33bde92c066e7135fbaf2fe40806f5bbceb9
pablojwittich <pablojwittich1998@gmail.com>
2024-10-28 21:19:09 -0300

    new project

---
+++ Ato-laravel/resources/views/auth/register.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Sign In</title>
+        <style>
+        </style>
+    </head>
+    <body>
+        <form method="POST" action="{{route('register.user')}}" >
+    </body>
+</html>
---
+++ Ato-laravel/resources/views/index.blade.php
@@ -0,0 +1,17 @@
+@include('app')
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>A tus ordenes</title>
+        <style>
+
+        </style>
+    </head>
+    <body>
+        <header>
+            <h1>A tus ordenes</h1>
+        </header>
+    </body>
+</html>
