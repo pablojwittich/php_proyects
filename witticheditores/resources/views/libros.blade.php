e720a9d6b7e46b4a5eb476822908976d44490a01
Pablo Wittich <43766694+pablojwittich@users.noreply.github.com>
2026-04-09 17:09:39 -0300

    Delete witticheditores directory

--- witticheditores/resources/views/libros.blade.php
+++
@@ -1,2 +0,0 @@
-@include('app')
-<h2 style="text-align: center; margin-top:1em;">Libros</h2>
--- witticheditores/storage/framework/views/956e516ea6ef117884fa59be1f1e4dbd.php
+++
@@ -1,1 +0,0 @@
-<?php echo $__env->make('app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /home/p4bl0/Documentos/php_proyects/witticheditores/resources/views/login.blade.php ENDPATH**/ ?>
